eed0e761d3bc82be0bc3e45aec48897163a0090c
Joe Catera <zhouyaoji@gmail.com>
2026-08-24 20:00:42 -0700

    Add landing-page links to documentation sites

--- sites/sphinx-rest/conf.py
+++ sites/sphinx-rest/conf.py
@@ -5,5 +5,10 @@ copyright = "2026, Joe Catera"
 extensions = []
 exclude_patterns = []
 html_theme = "alabaster"
+html_theme_options = {
+    "extra_nav_links": {
+        "All renderers": "https://zhouyaoji.github.io/northstar-docs-frameworks/"
+    }
+}
 html_title = "Northstar · Sphinx reST"
 html_baseurl = "https://zhouyaoji.github.io/northstar-docs-frameworks/sphinx-rest/"
